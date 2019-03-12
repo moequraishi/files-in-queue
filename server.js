6a2f223bb6abc693af76982b5eef9d7afa56badb
Moe <moe.quraishi@allheart.com>
2019-03-12 12:42:03 -0700

    removing pseudo comments

--- server.js
+++ server.js
@@ -1,36 +1,22 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const path = require('path');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const routes = require('./server/routes/routes');
 const app = express();
 const port = 1337;
 
-// Setting the Path for Compiled Angular Folder
-app.set('dist', path.join(__dirname, 'dist/restful-crud'));
-
-// Body Parser used to extract json
+app.set('dist', path.join(__dirname, 'dist/inqueue'));
+app.use(express.static(path.join(__dirname, 'dist/inqueue')));
 app.use(bodyParser.json());
-
-// See https://www.npmjs.com/package/body-parser#bodyparserurlencodedoptions
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// CORS Module to set headers for us with default options of allowing all origins
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(routes);
 app.use(cors());
 
-// Setting Express Static files directory - angular's compiled folder
-app.use(express.static(path.join(__dirname, 'dist/restful-crud')));
-
-// Setting the Express Routes
-app.use(routes);
-
-// Get all routes, and send the compiled index.html file to use Angular's routing instead
-// this fixes the refresh issue since an Angular route does not exist in the backend
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/restful-crud/index.html'))
+  res.sendFile(path.join(__dirname, 'dist/inqueue/index.html'));
 });
 
-// Listening on the port/server for any changes
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`)
+  console.log(`Listening on port: ${port}`);
 });
